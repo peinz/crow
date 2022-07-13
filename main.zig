e8eb7b7c456475cc6a582eec7243e89e1eefafd1
peinz <philippHeinz66@gmail.com>
2022-07-13 22:37:56 +0200

    add print function

--- main.zig
+++ main.zig
@@ -39,6 +39,31 @@ const Block = struct {
     statements: []const Statement,
 };
 
+const FooErrors = error{
+    DiskQuota,
+    FileTooBig,
+    InputOutput,
+    NoSpaceLeft,
+    AccessDenied,
+    BrokenPipe,
+    SystemResources,
+    OperationAborted,
+    NotOpenForWriting,
+    WouldBlock,
+    ConnectionResetByPeer,
+    Unexpected,
+};
+
+fn printIndented(comptime format: []const u8, args: anytype, indent: i8) FooErrors!void {
+    const stdout = std.io.getStdOut().writer();
+    var i: u8 = 0;
+    while (i < indent) {
+        i += 1;
+        try stdout.print(" ", .{});
+    }
+    try stdout.print(format, args);
+}
+
 const Expression = union(enum) {
     literal: Literal,
     variable: Variable,
@@ -76,6 +101,33 @@ const Expression = union(enum) {
             },
         };
     }
+    pub fn dump(self: @This(), indent: i8) FooErrors!void {
+        _ = indent;
+        _ = switch (self) {
+            .literal => try printIndented("literal({d})\n", .{self.literal.value}, indent),
+            .variable => try printIndented("variable('{s}')\n", .{self.variable.name}, indent),
+            .binaryExpression => {
+                try printIndented("binaryExpression('{s}')\n", .{self.binaryExpression.opr}, indent);
+                try printIndented("lhs:\n", .{}, indent + 2);
+                try self.binaryExpression.lhs.dump(indent + 4);
+                try printIndented("rhs:\n", .{}, indent + 2);
+                try self.binaryExpression.rhs.dump(indent + 4);
+            },
+            .block => {
+                try printIndented("block\n", .{}, indent);
+                for (self.block.statements) |statement| {
+                    if (statement == Statement.constantDeclaration) {
+                        try printIndented("- constantDeclaration('{s}')\n", .{statement.constantDeclaration.constantName}, indent + 2);
+                        try statement.constantDeclaration.expression.dump(indent + 6);
+                    }
+                    if (statement == Statement.returnStatement) {
+                        try printIndented("- returnStatement\n", .{}, indent + 2);
+                        try statement.returnStatement.expression.dump(indent + 6);
+                    }
+                }
+            },
+        };
+    }
 };
 
 const Statement = union(enum) {
@@ -104,6 +156,8 @@ pub fn main() !void {
     const context = Context.init(&scope);
 
     const result = bE.evaluate(context);
+
+    try bE.dump(0);
 
     const stdout = std.io.getStdOut().writer();
     try stdout.print("Hello, {d}!\n", .{result});
